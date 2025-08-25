aad4d7578659125102911573fffcf92544059a9e
Ogabek Bakhodirov <ogabek.bakhodirov@gmail.com>
2025-08-26 04:47:03 +0500

    resolved

---
+++ Shizen/Shizen/Model/GlobalVariable.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalVariable.swift
+//  Shizen
+//
+//  Created by Ogabek Bakhodirov on 26/08/25.
+//
+
+import UIKit
+import ManagedSettings
+
+let USER_DEFAULTS = UserDefaults.standard
+
+let AppWindowStateKey = "AppWindowState"
+
+public let windowHeight = UIScreen.main.bounds.height
+public let windowWidth =  UIScreen.main.bounds.width
